458b73e1d5adcb965e6d5e859f46771bd43aec0a
5h4rks <randres1211@gmail.com>
2025-05-06 21:42:18 -0500

    Mateo y Roller

--- Frontend/js/login.js
+++ Frontend/js/login.js
@@ -3,6 +3,7 @@ const adminModal = document.querySelector("#adminModal");
 const closebtn = document.querySelector("#admin-cancel");
 const dialog = document.querySelector("#adminForm");
 const loginbtn = document.querySelector("#admin-login");
+const ingresar = document.querySelector("#btn-ingresar");   
 
 btnadminmodal.addEventListener("click", function () {
     adminModal.showModal();
@@ -17,22 +18,54 @@ loginbtn.addEventListener("click", function (e) {
 
     const credencial = document.querySelector("#admin-credencial").value;
 
-    fetch("", {
+    fetch("http://127.0.0.1:8000/admin_login/", {
         method: "POST",
-        headers:{
+        headers: {
             "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+            "password": credencial,
+        }),
+    })
+    
+    .then(res => {
+        if (res.status === 200){
+            alert("Credenciales correctas, redirigiendo a la página de administración...");
+        } else {
+            alert("Credenciales incorrectas");
         }
-})
-.then(res => res.json())
-.then(data => {
-    if (data.status === true) {
-        
-    } else {
-        alert("Credenciales incorrectas");
-    }
-})
-
-.catch(error => {
-    console.error("Error:", error);
+    })
+    .catch(error => {
+        console.error("Error:", error);
+    });
 });
+
+
+
+ingresar.addEventListener("click", function (e) {
+    e.preventDefault();
+
+    const credencial = document.querySelector("#credencial").value;
+    const rol = document.querySelector("#userType").value;
+    fetch("http://127.0.0.1:8000/rol_login/", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+            "rol": rol,
+            "password": credencial,
+        }),
+    })
+    
+    .then(res => {
+        if (res.status === 200){
+            alert("Credenciales correctas, redirigiendo a la página de administración...");
+        } else {
+            alert("Credenciales incorrectas");
+        }
+    })
+    .catch(error => {
+        console.error("Error:", error);
+    });
 });
